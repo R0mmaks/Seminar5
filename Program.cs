c36661a97944d302d3780370c7116b5b1555fd39
Rommaks <romanmaksin22@gmail.com>
2023-06-22 22:40:47 +0300

    задачи 34, 36, 38

--- Program.cs
+++ Program.cs
@@ -1,40 +1,40 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными 
-// трёхзначными числами. Напишите программу, которая покажет 
-// количество чётных чисел в массиве.
-// [345, 897, 568, 234] -> 2
+﻿Задача 34: Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, которая покажет 
+количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
 
-// Console.Write("Введите количество элементов массива: ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// int[] randomArray = new int[a];
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
 
-// void mas(int a)
-// {
-// for (int i = 0; i < a; i++)
-// {
-// randomArray[i] = new Random().Next(99,999);
-// Console.Write(randomArray[i] + " ");
+void mas(int a)
+{
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = new Random().Next(99,999);
+Console.Write(randomArray[i] + " ");
 
-// }
-// Console.WriteLine ();
-// }
+}
+Console.WriteLine ();
+}
 
-// int kol(int[] randomArray)
-// {
-// int kol = 0;
-// for (int i = 0; i < randomArray.Length; i++)
-// {
-// if (randomArray[i] % 2 == 0)
-// kol = kol + 1;
-// }
-// return kol;
-// }
+int kol(int[] randomArray)
+{
+int kol = 0;
+for (int i = 0; i < randomArray.Length; i++)
+{
+if (randomArray[i] % 2 == 0)
+kol = kol + 1;
+}
+return kol;
+}
 
-// mas(a);
+mas(a);
 
 
-// Задача 36: Задайте одномерный массив, 
-// заполненный случайными числами.Найдите 
-// сумму элементов, стоящих на нечётных позициях.
+Задача 36: Задайте одномерный массив, 
+заполненный случайными числами.Найдите 
+сумму элементов, стоящих на нечётных позициях.
 
 
 Console.WriteLine("Введите размер массива");
@@ -66,3 +66,45 @@ void PrintArray(int[] numbers)
 
     Console.WriteLine();
 }
+
+
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] randomArray = new double[a];
+
+void mas(int a)
+{
+Random rand = new Random();
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = rand.NextDouble();
+Console.WriteLine($"{randomArray[i]:F2} ");
+}
+Console.WriteLine ();
+}
+
+double raz(double[] randomArray)
+{
+double min = randomArray[0];
+double max = randomArray[0];
+int i = 1;
+while (i < randomArray.Length)
+{
+if (max<randomArray[i])
+max = randomArray[i];
+if (min>randomArray[i])
+min = randomArray[i];
+i = i + 1;
+}
+Console.WriteLine($"минимальное число= {min}");
+Console.WriteLine($"максимальное число= {max}");
+return max-min;
+
+}
+
+mas(a);
+
+Console.Write($"Разница между максимальным и минимальным элементами массива: {raz(randomArray):F2}");
